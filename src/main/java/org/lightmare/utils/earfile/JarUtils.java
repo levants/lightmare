5887d5b696fcba9816385ec8ceb28009742dced3
levan <levantsinadze@gmail.com>
2013-01-10 15:26:26 +0400

    Added JarUtils extension of IOUtils for jar files

---
+++ src/main/java/org/lightmare/utils/earfile/JarUtils.java
@@ -0,0 +1,70 @@
+package org.lightmare.utils.earfile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.lightmare.jpa.ConfigLoader;
+import org.lightmare.utils.IOUtils;
+
+public class JarUtils extends IOUtils {
+
+	public JarUtils(String path) {
+		super(path);
+	}
+
+	public JarUtils(File file) {
+		super(file);
+	}
+
+	public JarUtils(URL url) throws IOException {
+		super(url);
+	}
+
+	@Override
+	public InputStream earReader() throws IOException {
+		return null;
+	}
+
+	@Override
+	public void getEjbLibs() throws IOException {
+
+	}
+
+	@Override
+	public void extractEjbJars(Set<String> jarNames) throws IOException {
+
+		URL currentURL = realFile.toURI().toURL();
+		getEjbURLs().add(currentURL);
+
+		boolean checkOnOrm = checkOnOrm(path);
+		if (xmlFromJar && checkOnOrm) {
+			String xmlPath = String.format("%s!/%s", currentURL.toString(),
+					ConfigLoader.XML_PATH);
+			URL xmlURL = new URL("jar", "", xmlPath);
+			getXmlFiles().put(realFile.getName(), xmlURL);
+			getXmlURLs().put(currentURL, xmlURL);
+		}
+	}
+
+	@Override
+	public boolean checkOnOrm(String jarName) throws IOException {
+		ZipFile zipFile = new ZipFile(realFile);
+		ZipEntry xmlEntry = zipFile.getEntry(ConfigLoader.XML_PATH);
+
+		return xmlEntry != null;
+	}
+
+	@Override
+	public void scan(Object... args) throws IOException {
+		if (args.length > 0) {
+			xmlFromJar = (Boolean) args[0];
+		}
+		extractEjbJars(Collections.<String> emptySet());
+	}
+}
