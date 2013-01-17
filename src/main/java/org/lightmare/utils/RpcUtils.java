379804b8dd58f3fd5879913c91fa31460fcb624d
levan <levantsinadze@gmail.com>
2013-01-17 22:25:05 +0400

    added dependencies for JSON serialization JaxRS with embeddable web server

--- src/main/java/org/lightmare/utils/RpcUtils.java
+++ src/main/java/org/lightmare/utils/RpcUtils.java
@@ -12,9 +12,14 @@ import org.lightmare.ejb.EjbConnector;
 import org.lightmare.remote.rpc.RPCall;
 import org.lightmare.remote.rpc.wrappers.RpcWrapper;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 /**
- * Listener class for serialization and de-serialization of objects and call
- * bean {@link Method}s connection to bean remotely
+ * Listener class for serialization and de-serialization (both java and json) of
+ * objects and call bean {@link Method}s connection to bean remotely
  * 
  * @author Levan
  * 
@@ -26,6 +31,22 @@ public class RpcUtils {
 	public static final int INT_SIZE = 4;
 
 	public static final int BYTE_SIZE = 1;
+
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+
+	private static boolean mapperConfigured;
+
+	private static ObjectMapper getMapper() {
+		if (mapperConfigured) {
+			return MAPPER;
+		} else {
+			synchronized (RpcUtils.class) {
+				MAPPER.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+			}
+
+			return MAPPER;
+		}
+	}
 
 	public static byte[] serialize(Object value) throws IOException {
 
@@ -42,6 +63,20 @@ public class RpcUtils {
 		} finally {
 			stream.close();
 			objectStream.close();
+		}
+	}
+
+	public static <T> String write(Object value) throws IOException {
+		String data;
+		try {
+			data = getMapper().writeValueAsString(value);
+			return data;
+		} catch (JsonGenerationException ex) {
+			throw new IOException(ex);
+		} catch (JsonMappingException ex) {
+			throw new IOException(ex);
+		} catch (IOException ex) {
+			throw new IOException(ex);
 		}
 	}
 
@@ -62,6 +97,21 @@ public class RpcUtils {
 		} finally {
 			stream.close();
 			objectStream.close();
+		}
+	}
+
+	public static <T> T read(String data, Class<T> valueClass)
+			throws IOException {
+		T value;
+		try {
+			value = getMapper().readValue(data, valueClass);
+			return value;
+		} catch (JsonGenerationException ex) {
+			throw new IOException(ex);
+		} catch (JsonMappingException ex) {
+			throw new IOException(ex);
+		} catch (IOException ex) {
+			throw new IOException(ex);
 		}
 	}
 
