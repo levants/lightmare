4edeec571a4ce95c1d055ec3d6b4500f4e765332
levan <levantsinadze@gmail.com>
2013-02-21 23:29:13 +0400

    organized imports in DSContext class

--- src/main/java/org/lightmare/jndi/DSContext.java
+++ src/main/java/org/lightmare/jndi/DSContext.java
@@ -8,8 +8,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import org.lightmare.ejb.EjbConnector;
-import org.lightmare.ejb.meta.MetaContainer;
-import org.lightmare.ejb.meta.MetaData;
 import org.lightmare.jpa.JPAManager;
 import org.osjava.sj.memory.MemoryContext;
 
