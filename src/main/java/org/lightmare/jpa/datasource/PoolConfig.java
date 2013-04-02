3fbc777e5222ae0f4f8ff21916fd3249aa9c4a37
levan <levantsinadze@gmail.com>
2013-04-02 21:04:46 +0400

    max timeout for c3p0 pool is disabled by default

--- src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
@@ -18,7 +18,7 @@ public class PoolConfig {
     public static final String MAX_POOL_SIZE_DEF_VALUE = "15";
     public static final String INITIAL_POOL_SIZE_DEF_VALUE = "5";
     public static final String MIN_POOL_SIZE_DEF_VALUE = "5";
-    public static final String MAX_IDLE_TIMEOUT_DEF_VALUE = "1800";
+    public static final String MAX_IDLE_TIMEOUT_DEF_VALUE = "0";
     public static final String MAX_STATEMENTS_DEF_VALUE = "50";
     public static final String AQUIRE_INCREMENT_DEF_VALUE = "50";
 }
