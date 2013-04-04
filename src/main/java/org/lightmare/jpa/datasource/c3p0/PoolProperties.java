590b0365c217e1c04b32cf28da1f9a2eb2f74454
levan <levantsinadze@gmail.com>
2013-04-04 11:35:34 +0400

    added feature to read pool properties from given path

---
+++ src/main/java/org/lightmare/jpa/datasource/c3p0/PoolProperties.java
@@ -0,0 +1,36 @@
+package org.lightmare.jpa.datasource.c3p0;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.lightmare.utils.ObjectUtils;
+
+/**
+ * Searchs properties files for c3p0 pool and loads them
+ * 
+ * @author levan
+ * 
+ */
+public class PoolProperties {
+
+    public static Properties load(String path) throws IOException {
+
+	ClassLoader loader = Thread.currentThread().getContextClassLoader();
+	if (!ObjectUtils.available(path)) {
+	    path = "META-INF/pool.properties";
+	}
+	InputStream stream = loader.getResourceAsStream(path);
+	try {
+	    Properties properties = new Properties();
+	    properties.load(stream);
+
+	    return properties;
+	} finally {
+	    if (ObjectUtils.notNull(stream)) {
+		stream.close();
+	    }
+	}
+
+    }
+}
