09446a488c2cc5aae2083291fec738d209c40d10
levan <levantsinadze@gmail.com>
2013-04-09 20:49:23 +0400

    added test cases for Path object

--- src/test/java/org/lightmare/deploy/WatchTest.java
+++ src/test/java/org/lightmare/deploy/WatchTest.java
@@ -3,19 +3,38 @@ package org.lightmare.deploy;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.Iterator;
 
 import org.junit.Test;
+import org.lightmare.utils.ObjectUtils;
 
 public class WatchTest {
 
     @Test
-    public void pathTest(){
-	
-	FileSystem fs = FileSystems.getDefault();
-	Path deployPath = fs.getPath("./lib");
-	String fileName = deployPath.getFileName().toString();
-	String pathString = deployPath.toString();
-	System.out.println(fileName);
-	System.out.println(pathString);
+    public void pathTest() {
+
+	try {
+	    FileSystem fs = FileSystems.getDefault();
+	    Path deployPath = fs.getPath("./lib");
+	    String fileName = deployPath.getFileName().toString();
+	    String pathString = deployPath.toString();
+	    System.out.println(fileName);
+	    System.out.println(pathString);
+	    System.out.println(deployPath.isAbsolute());
+	    Path root = deployPath.getRoot();
+	    if (ObjectUtils.notNull(root)) {
+		System.out.println(root.toString());
+	    }
+	    Iterator<Path> iterator = deployPath.iterator();
+	    while (iterator.hasNext()) {
+		System.out.println(iterator.next());
+	    }
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
+    }
+
+    public void watchTest() {
+
     }
 }
