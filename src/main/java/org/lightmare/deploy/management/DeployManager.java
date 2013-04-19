2aae68b01805a32c150906f56581810de6f129d5
levan <levantsinadze@gmail.com>
2013-04-19 18:41:57 +0400

    authentication of DeployManager servlet class

--- src/main/java/org/lightmare/deploy/management/DeployManager.java
+++ src/main/java/org/lightmare/deploy/management/DeployManager.java
@@ -3,6 +3,7 @@ package org.lightmare.deploy.management;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.io.Serializable;
 import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.lightmare.cache.MetaContainer;
@@ -70,7 +72,39 @@ public class DeployManager extends HttpServlet {
 
     private static final String END_PAGE = "</body></table>\n" + "</html>";
 
+    private static final String LOGIN_PAGE = "<html>\n" + "\t\t<head>\n"
+	    + "\t\t\t<title>Login</title>\n" + "\t\t</head>\n" + "\t\t<body>\n"
+	    + "\t\t\t<table>\n" + "\t\t\t\t<tr>\n" + "\t\t\t\t\t<td>\n"
+	    + "\t\t\t\t\t\t<form name = \"ManagementLogin\" method=\"post\">"
+	    + "\t\t\t\t\t\t\t<input type=\"user\" name=\"user\">"
+	    + "\t\t\t\t\t\t\t<input type=\"password\" name=\"password\">"
+	    + "\t\t\t\t\t\t\t<input type=\"submit\" value=\"Submit\">"
+	    + "\t\t\t\t\t\t</form>" + "\t\t\t\t\t</td>\n" + "\t\t\t\t</tr>\n";
+
+    private static final String INCORRECT_MESSAGE = "<tr><td><b>invalid user name / passowd</b></td></tr>";
+
+    private static final String END_LOGIN_PAGE = "\t\t\t</table>\n" + "</html>";
+
+    private static final String USER_PARAMETER_NAME = "user";
+
+    private static final String PASS_PARAMETER_NAME = "password";
+
+    private static final String DEPLOY_PASS_KEY = "deploy_manager_pass";
+
     private static final Logger LOG = Logger.getLogger(DeployManager.class);
+
+    /**
+     * Class to cache authenticated users for {@link DeployManager} servlet page
+     * 
+     * @author levan
+     * 
+     */
+    private static class DeployPass implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+    }
 
     /**
      * To filter only deployed sub files from directory
@@ -169,26 +203,101 @@ public class DeployManager extends HttpServlet {
 	Watcher.undeployFile(url);
     }
 
+    private String toLoginPage(boolean incorrect) {
+
+	StringBuilder builder = new StringBuilder();
+	builder.append(LOGIN_PAGE);
+	if (incorrect) {
+	    builder.append(INCORRECT_MESSAGE);
+	}
+	builder.append(END_LOGIN_PAGE);
+
+	return builder.toString();
+    }
+
+    private boolean authenticate(String userName, String password,
+	    HttpSession session) {
+
+	boolean valid;
+
+	DeployPass pass = new DeployPass();
+	pass.userName = userName;
+	session.setAttribute(DEPLOY_PASS_KEY, pass);
+	valid = Boolean.TRUE;
+
+	return valid;
+
+    }
+
+    private boolean check(HttpSession session) {
+
+	boolean valid = ObjectUtils.notNull(session);
+	if (valid) {
+	    Object pass = session.getAttribute(DEPLOY_PASS_KEY);
+	    valid = ObjectUtils.notNull(pass);
+	    if (valid) {
+		valid = (pass instanceof DeployPass)
+			&& (ObjectUtils.available(((DeployPass) pass).userName));
+	    }
+	}
+
+	return valid;
+    }
+
     @Override
     protected void doGet(HttpServletRequest request,
 	    HttpServletResponse response) throws ServletException, IOException {
-	String fileName = request.getParameter(REDEPLOY_PARAM_NAME);
-	String type = request.getParameter(TYPE_PARAM_NAME);
-	if (ObjectUtils.available(fileName)) {
-	    URL url = getURL(fileName);
-	    if (type == null || REDEPLOY_TYPE.equals(type)) {
-		redeploy(url);
-	    } else if (UNDEPLOY_TYPE.equals(type)) {
-		undeploy(url);
+
+	boolean check = check(request.getSession(Boolean.FALSE));
+	String html;
+	if (check) {
+
+	    String fileName = request.getParameter(REDEPLOY_PARAM_NAME);
+	    String type = request.getParameter(TYPE_PARAM_NAME);
+	    if (ObjectUtils.available(fileName)) {
+		URL url = getURL(fileName);
+		if (type == null || REDEPLOY_TYPE.equals(type)) {
+		    redeploy(url);
+		} else if (UNDEPLOY_TYPE.equals(type)) {
+		    undeploy(url);
+		}
 	    }
+	    html = getApplications();
+	} else {
+	    html = toLoginPage(Boolean.FALSE);
 	}
-	String html = getApplications();
 
 	Writer writer = response.getWriter();
 	try {
 	    writer.write(html);
 	} finally {
 	    writer.close();
+	}
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+
+	String userName = request.getParameter(USER_PARAMETER_NAME);
+	String password = request.getParameter(PASS_PARAMETER_NAME);
+	boolean valid = ObjectUtils.available(userName)
+		&& ObjectUtils.available(password);
+	if (valid) {
+	    valid = authenticate(userName, password,
+		    request.getSession(Boolean.TRUE));
+	}
+	if (valid) {
+	    response.sendRedirect("DeployManager");
+	} else {
+
+	    String html = toLoginPage(Boolean.TRUE);
+	    Writer writer = response.getWriter();
+	    try {
+		writer.write(html);
+	    } finally {
+		writer.close();
+	    }
 	}
     }
 }
