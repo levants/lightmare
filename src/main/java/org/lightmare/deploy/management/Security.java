7665014ff58f2a4422c15cb22ad57cf68e80d5cb
levan <levantsinadze@gmail.com>
2013-04-21 13:56:24 +0400

    added Security class to authorize users

---
+++ src/main/java/org/lightmare/deploy/management/Security.java
@@ -0,0 +1,68 @@
+package org.lightmare.deploy.management;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.lightmare.config.Configuration;
+import org.lightmare.deploy.MetaCreator;
+import org.lightmare.utils.ObjectUtils;
+
+/**
+ * Manages and administrator users
+ * 
+ * @author Levan
+ * 
+ */
+public class Security {
+
+    private Properties cache;
+
+    public Security() throws IOException {
+	cacheUsers();
+    }
+
+    private void loadUsers(File file) throws IOException {
+
+	InputStream stream = new FileInputStream(file);
+	cache = new Properties();
+	try {
+	    cache.load(stream);
+	} finally {
+	    if (ObjectUtils.notNull(stream)) {
+		stream.close();
+	    }
+	}
+    }
+
+    public void cacheUsers() throws IOException {
+
+	String path = MetaCreator.CONFIG
+		.getStringValue(Configuration.ADMIN_USERS_PATH);
+	if (ObjectUtils.notAvailable(path)) {
+	    path = Configuration.ADMIN_USERS_PATH_DEF;
+	}
+
+	File file = new File(path);
+	if (file.exists()) {
+	    loadUsers(file);
+	}
+    }
+
+    public boolean authenticate(String user, String pass) {
+
+	boolean valid;
+	if (ObjectUtils.available(cache)) {
+
+	    String cachedPass = (String) cache.get(user);
+	    valid = (ObjectUtils.available(cachedPass) && cachedPass
+		    .equals(pass));
+	} else {
+	    valid = Boolean.TRUE;
+	}
+
+	return valid;
+    }
+}
