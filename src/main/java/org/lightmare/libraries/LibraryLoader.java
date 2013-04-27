8d4f2f13e4cb9d23896bde8d6f64e912fb0181c7
levan <levantsinadze@gmail.com>
2013-04-27 20:16:27 +0400

    performed clearAssertionStatus on class loader at undeploy time

--- src/main/java/org/lightmare/libraries/LibraryLoader.java
+++ src/main/java/org/lightmare/libraries/LibraryLoader.java
@@ -79,6 +79,7 @@ public class LibraryLoader {
 
 	if (ObjectUtils.notNull(loader) && loader instanceof URLClassLoader) {
 	    ((URLClassLoader) loader).close();
+	    loader.clearAssertionStatus();
 	}
     }
 
