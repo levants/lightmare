805215870a9bde454f6eea9b87cf842a371c8530
levan <levantsinadze@gmail.com>
2013-06-20 19:31:51 +0400

    added javax.ejb.spi.EJBContainerProvider implementation

---
+++ src/main/java/org/lightmare/ejb/embeddable/EJBContainerProviderImpl.java
@@ -0,0 +1,24 @@
+package org.lightmare.ejb.embeddable;
+
+import java.util.Map;
+
+import javax.ejb.EJBException;
+import javax.ejb.embeddable.EJBContainer;
+import javax.ejb.spi.EJBContainerProvider;
+
+/**
+ * Implementation for {@link javax.ejb.spi.EJBContainerProvider} interface for
+ * lightmare ejb container
+ * 
+ * @author levan
+ * 
+ */
+public class EJBContainerProviderImpl implements EJBContainerProvider {
+
+    @Override
+    public EJBContainer createEJBContainer(Map<?, ?> properties)
+	    throws EJBException {
+
+	return new EJBContainerImpl(properties);
+    }
+}
