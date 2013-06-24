bf2e392ff94308e89a866cd657a5e7bead4658c8
levan <levantsinadze@gmail.com>
2013-06-24 21:27:54 +0400

    improved entity stream for REST Inflector implementation class

--- src/main/java/org/lightmare/rest/providers/RestInflector.java
+++ src/main/java/org/lightmare/rest/providers/RestInflector.java
@@ -1,25 +1,29 @@
 package org.lightmare.rest.providers;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.Response.Status.Family;
-import javax.ws.rs.core.Response.StatusType;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.MessageBodyReader;
 
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
 import org.glassfish.jersey.message.MessageBodyWorkers;
 import org.glassfish.jersey.process.Inflector;
 import org.glassfish.jersey.server.ContainerRequest;
@@ -47,37 +51,10 @@ public class RestInflector implements
 
     private static final int PARAMS_DEF_LENGTH = 0;
 
+    private static final int ZERO_AVAILABLE_STREAM = 0;
+
     @Context
     private MessageBodyWorkers workers;
-
-    /**
-     * Error status for exception handling
-     * 
-     * @author Levan
-     * 
-     */
-    protected static class ErrorStatusType implements StatusType {
-
-	private String message;
-
-	public ErrorStatusType(String message) {
-	    this.message = message;
-	}
-
-	public int getStatusCode() {
-	    return Status.ACCEPTED.getStatusCode();
-	}
-
-	@Override
-	public String getReasonPhrase() {
-	    return message;
-	}
-
-	@Override
-	public Family getFamily() {
-	    return Family.SERVER_ERROR;
-	}
-    }
 
     private static final Logger LOG = Logger.getLogger(RestInflector.class);
 
@@ -102,6 +79,70 @@ public class RestInflector implements
 	return mediaType;
     }
 
+    protected void copyAll(MultivaluedMap<String, String> from,
+	    MultivaluedMap<String, String> to) {
+
+	for (Map.Entry<String, List<String>> entry : from.entrySet()) {
+
+	    to.addAll(entry.getKey(), entry.getValue());
+	}
+    }
+
+    protected void addAll(MultivaluedMap<String, String> from,
+	    MultivaluedMap<String, String> to) {
+
+	if (ObjectUtils.available(from)) {
+	    copyAll(from, to);
+	}
+    }
+
+    protected MultivaluedMap<String, String> extractParameters(
+	    ContainerRequestContext request) {
+
+	MultivaluedMap<String, String> params = new MultivaluedStringMap();
+	MultivaluedMap<String, String> exts;
+
+	UriInfo uriInfo = request.getUriInfo();
+	exts = request.getHeaders();
+	addAll(exts, params);
+	exts = uriInfo.getPathParameters();
+	addAll(exts, params);
+	exts = uriInfo.getQueryParameters();
+	addAll(exts, params);
+
+	return params;
+    }
+
+    protected void writeParams(StringBuilder builder, List<String> params) {
+
+	Iterator<String> iterator = params.iterator();
+	while (iterator.hasNext()) {
+	    if (builder.length() > 0) {
+		builder.append(" ");
+	    }
+	    builder.append(iterator.next());
+	}
+    }
+
+    protected void createStream(ContainerRequestContext request)
+	    throws IOException {
+
+	MultivaluedMap<String, String> params = extractParameters(request);
+	InputStream entityStream = request.getEntityStream();
+	if (ObjectUtils.available(params)
+		&& (!request.hasEntity() || entityStream.available() == ZERO_AVAILABLE_STREAM)) {
+	    List<String> paramValues;
+	    StringBuilder paramBuilder = new StringBuilder();
+	    for (Parameter parameter : parameters) {
+		paramValues = params.get(parameter.getSourceName());
+		writeParams(paramBuilder, paramValues);
+	    }
+	    byte[] bts = paramBuilder.toString().getBytes();
+	    ByteArrayInputStream ios = new ByteArrayInputStream(bts);
+	    request.setEntityStream(ios);
+	}
+    }
+
     @SuppressWarnings({ "unchecked", "rawtypes" })
     private Object[] getParameters(ContainerRequestContext data)
 	    throws IOException {
@@ -109,6 +150,7 @@ public class RestInflector implements
 	Object[] params;
 	ContainerRequest request = (ContainerRequest) data;
 	if (ObjectUtils.available(parameters)) {
+	    createStream(request);
 	    List<Object> paramsList = new ArrayList<Object>();
 	    MessageBodyReader<?> reader;
 	    Class<?> rawType;
@@ -146,22 +188,22 @@ public class RestInflector implements
     @Override
     public Response apply(ContainerRequestContext data) {
 
-	ResponseBuilder responseBuilder;
+	Response response;
 	Object value = null;
 	try {
 	    EjbConnector connector = new EjbConnector();
 	    InvocationHandler handler = connector.getHandler(metaData);
 	    Object bean = connector.connectToBean(metaData);
 	    Object[] params = getParameters(data);
-
 	    value = handler.invoke(bean, method, params);
-	    responseBuilder = Response.ok(value);
+	    response = Response.ok(value).build();
 	} catch (Throwable ex) {
 	    LOG.error(ex.getMessage(), ex);
-	    responseBuilder = Response.status(
-		    new ErrorStatusType(ex.getMessage())).entity(ex.toString());
+	    WebApplicationException webEx = new WebApplicationException(ex);
+	    webEx.fillInStackTrace();
+	    response = webEx.getResponse();
 	}
 
-	return responseBuilder.build();
+	return response;
     }
 }
