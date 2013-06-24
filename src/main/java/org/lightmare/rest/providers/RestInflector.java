ca3ec9758b61b925d351c6c3faec9d40a2bf5153
levan <levantsinadze@gmail.com>
2013-06-24 21:28:23 +0400

    organized import for REST handler

--- src/main/java/org/lightmare/rest/providers/RestInflector.java
+++ src/main/java/org/lightmare/rest/providers/RestInflector.java
@@ -7,7 +7,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
