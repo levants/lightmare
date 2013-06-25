53c6b4d8b3c41e9e5035bc343e023783ba84b42e
levan <levantsinadze@gmail.com>
2013-06-25 14:45:32 +0400

    added PUT support for LightmareBean class for RESTFUL web services test cases

--- src/test/java/org/lightmare/bean/LightMareBean.java
+++ src/test/java/org/lightmare/bean/LightMareBean.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -51,7 +52,7 @@ public class LightMareBean implements LightMareBeanRemote {
 
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
-    // @PUT
+    @PUT
     // @Produces("application/json;charset=utf-8")
     // @Consumes("application/json;charset=utf-8")
     public void addPerson(@QueryParam("person") Person person) {
