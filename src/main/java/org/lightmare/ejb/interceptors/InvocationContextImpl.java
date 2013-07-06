d8422327181a44ef02229bb7f37816f79c552c9e
levan <levantsinadze@gmail.com>
2013-07-06 20:05:20 +0400

    added InvocationContext implementation

---
+++ src/main/java/org/lightmare/ejb/interceptors/InvocationContextImpl.java
@@ -0,0 +1,77 @@
+package org.lightmare.ejb.interceptors;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+import javax.interceptor.InvocationContext;
+
+import org.lightmare.utils.reflect.MetaUtils;
+
+/**
+ * Implementation of {@link InvocationContext} for lightmare ejb interceptors
+ * 
+ * @author Levan
+ * 
+ */
+public class InvocationContextImpl implements InvocationContext {
+
+    private Queue<Method> methods = new LinkedList<Method>();
+
+    private Object[] parameters;
+
+    private Queue<Object> targets = new LinkedList<Object>();
+
+    private Map<String, Object> contextData = new HashMap<String, Object>();
+
+    public InvocationContextImpl(Queue<Method> methods, Object[] parameters,
+	    Queue<Object> targets, Map<String, Object> contextData) {
+	this.methods = methods;
+	this.parameters = parameters;
+	this.targets = targets;
+	this.contextData = contextData;
+    }
+
+    @Override
+    public Object getTarget() {
+
+	Object target = targets.peek();
+	return target;
+    }
+
+    @Override
+    public Method getMethod() {
+
+	return methods.peek();
+    }
+
+    @Override
+    public Object[] getParameters() {
+
+	return parameters;
+    }
+
+    @Override
+    public void setParameters(Object[] parameters) {
+
+	this.parameters = parameters;
+    }
+
+    @Override
+    public Map<String, Object> getContextData() {
+
+	return contextData;
+    }
+
+    @Override
+    public Object proceed() throws Exception {
+
+	Method method = methods.poll();
+	Object target = targets.poll();
+	Object value = MetaUtils.invoke(method, target, parameters);
+
+	return value;
+    }
+}
