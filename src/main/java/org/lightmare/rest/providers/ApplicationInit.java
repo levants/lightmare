069e609614bb5b74681c0c0d8f51890eeddeed49
levan <levantsinadze@gmail.com>
2013-07-09 18:23:44 +0400

    added some parameters to ApplicationInit class for REST services

--- src/main/java/org/lightmare/rest/providers/ApplicationInit.java
+++ src/main/java/org/lightmare/rest/providers/ApplicationInit.java
@@ -1,6 +1,13 @@
 package org.lightmare.rest.providers;
 
+import org.lightmare.rest.RestConfig;
+
 public class ApplicationInit {
 
-    public static final String APPLICATION_INIT_PARAM = "javax.ws.rs.Application";
+    public static final String REST_DEFAULT_URI = "/rest";
+
+    public static final String APPLICATION_INIT_PARAM_NAME = "javax.ws.rs.Application";
+
+    public static final String APPLICATION_INIT_PARAM = RestConfig.class
+	    .getName();
 }
