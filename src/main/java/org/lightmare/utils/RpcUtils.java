7f6fcf85737563958189c06b3196b625783bf9dc
levan <levantsinadze@gmail.com>
2013-07-25 22:51:41 +0400

    created JsonSerializer class and moved methods for JSON serialization from RpcUtils class

--- src/main/java/org/lightmare/utils/RpcUtils.java
+++ src/main/java/org/lightmare/utils/RpcUtils.java
@@ -9,11 +9,6 @@ import org.lightmare.remote.rpc.RPCall;
 import org.lightmare.remote.rpc.wrappers.RpcWrapper;
 import org.lightmare.utils.reflect.MetaUtils;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
 /**
  * Listener class for serialization and de-serialization (both java and json) of
  * objects and call bean {@link Method}s connection to bean remotely
@@ -28,81 +23,6 @@ public class RpcUtils {
     public static final int INT_SIZE = 4;
 
     public static final int BYTE_SIZE = 1;
-
-    private static final ObjectMapper MAPPER = new ObjectMapper();
-
-    private static boolean mapperConfigured;
-
-    private static void configureMapper() {
-
-	synchronized (RpcUtils.class) {
-	    if (ObjectUtils.notTrue(mapperConfigured)) {
-		MAPPER.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		mapperConfigured = Boolean.TRUE;
-	    }
-	}
-    }
-
-    /**
-     * Configures {@link ObjectMapper} if {@link RpcUtils#mapperConfigured} is
-     * <code>false</code>
-     * 
-     * @return {@link ObjectMapper}
-     */
-    private static ObjectMapper getMapper() {
-
-	if (ObjectUtils.notTrue(mapperConfigured)) {
-	    configureMapper();
-	}
-
-	return MAPPER;
-    }
-
-    /**
-     * Serializes {@link Object} to josn {@link String} with <a
-     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
-     * 
-     * @param value
-     * @return {@link String}
-     * @throws IOException
-     */
-    public static String write(Object value) throws IOException {
-	String data;
-	try {
-	    data = getMapper().writeValueAsString(value);
-	    return data;
-	} catch (JsonGenerationException ex) {
-	    throw new IOException(ex);
-	} catch (JsonMappingException ex) {
-	    throw new IOException(ex);
-	} catch (IOException ex) {
-	    throw new IOException(ex);
-	}
-    }
-
-    /**
-     * Deserializes josn {@link String} to {@link Object} with <a
-     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
-     * 
-     * @param data
-     * @param valueClass
-     * @return T
-     * @throws IOException
-     */
-    public static <T> T read(String data, Class<T> valueClass)
-	    throws IOException {
-	T value;
-	try {
-	    value = getMapper().readValue(data, valueClass);
-	    return value;
-	} catch (JsonGenerationException ex) {
-	    throw new IOException(ex);
-	} catch (JsonMappingException ex) {
-	    throw new IOException(ex);
-	} catch (IOException ex) {
-	    throw new IOException(ex);
-	}
-    }
 
     /**
      * Calls remote method for java RPC api
---
+++ src/main/java/org/lightmare/utils/serialization/JsonSerializer.java
@@ -0,0 +1,96 @@
+package org.lightmare.utils.serialization;
+
+import java.io.IOException;
+
+import org.lightmare.utils.ObjectUtils;
+import org.lightmare.utils.RpcUtils;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/**
+ * Reads write java objects from {@link Byte} array or {@link String} with java
+ * JSON serialization using jackson JSON library
+ * 
+ * @author Levan
+ * 
+ */
+public class JsonSerializer {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private static boolean mapperConfigured;
+
+    private static void configureMapper() {
+
+	synchronized (RpcUtils.class) {
+	    if (ObjectUtils.notTrue(mapperConfigured)) {
+		MAPPER.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		mapperConfigured = Boolean.TRUE;
+	    }
+	}
+    }
+
+    /**
+     * Configures {@link ObjectMapper} if {@link RpcUtils#mapperConfigured} is
+     * <code>false</code>
+     * 
+     * @return {@link ObjectMapper}
+     */
+    private static ObjectMapper getMapper() {
+
+	if (ObjectUtils.notTrue(mapperConfigured)) {
+	    configureMapper();
+	}
+
+	return MAPPER;
+    }
+
+    /**
+     * Serializes {@link Object} to josn {@link String} with <a
+     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
+     * 
+     * @param value
+     * @return {@link String}
+     * @throws IOException
+     */
+    public static String write(Object value) throws IOException {
+	String data;
+	try {
+	    data = getMapper().writeValueAsString(value);
+	    return data;
+	} catch (JsonGenerationException ex) {
+	    throw new IOException(ex);
+	} catch (JsonMappingException ex) {
+	    throw new IOException(ex);
+	} catch (IOException ex) {
+	    throw new IOException(ex);
+	}
+    }
+
+    /**
+     * Deserializes josn {@link String} to {@link Object} with <a
+     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
+     * 
+     * @param data
+     * @param valueClass
+     * @return T
+     * @throws IOException
+     */
+    public static <T> T read(String data, Class<T> valueClass)
+	    throws IOException {
+	T value;
+	try {
+	    value = getMapper().readValue(data, valueClass);
+	    return value;
+	} catch (JsonGenerationException ex) {
+	    throw new IOException(ex);
+	} catch (JsonMappingException ex) {
+	    throw new IOException(ex);
+	} catch (IOException ex) {
+	    throw new IOException(ex);
+	}
+    }
+}
