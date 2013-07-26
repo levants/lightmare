0681c137db5a99c88afb3a488487fb5cd429eab6
levan <levantsinadze@gmail.com>
2013-07-26 16:02:40 +0400

    addedcomments to ApplicationInit class

--- src/main/java/org/lightmare/rest/providers/ApplicationInit.java
+++ src/main/java/org/lightmare/rest/providers/ApplicationInit.java
@@ -2,6 +2,12 @@ package org.lightmare.rest.providers;
 
 import org.lightmare.rest.RestConfig;
 
+/**
+ * Default values for REST service application initialization
+ * 
+ * @author levan
+ * 
+ */
 public class ApplicationInit {
 
     public static final String REST_DEFAULT_URI = "/rest";
