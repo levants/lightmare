4801abe0d27dd88a0f6b7dd80635c98cc1fcc63b
levan <levantsinadze@gmail.com>
2013-07-30 13:24:52 +0400

    moved Transaction caching in new TransactionContainer class

---
+++ src/main/java/org/lightmare/cache/TransactionContainer.java
@@ -0,0 +1,48 @@
+package org.lightmare.cache;
+
+import javax.transaction.UserTransaction;
+
+/**
+ * Caches {@link javax.transaction.Transaction} objects for each method call in
+ * {@link ThreadLocal} cache
+ * 
+ * @author levan
+ * 
+ */
+public class TransactionContainer {
+
+    // Caches UserTransaction object per thread
+    private static final ThreadLocal<UserTransaction> TRANSACTION_HOLDER = new ThreadLocal<UserTransaction>();
+
+    /**
+     * Gets {@link UserTransaction} object from {@link ThreadLocal} per thread
+     * 
+     * @return {@link UserTransaction}
+     */
+    public static UserTransaction getTransaction() {
+
+	UserTransaction transaction = TRANSACTION_HOLDER.get();
+
+	return transaction;
+    }
+
+    /**
+     * Caches {@link UserTransaction} object in {@link ThreadLocal} per thread
+     * 
+     * @param transaction
+     */
+    public static void setTransaction(UserTransaction transaction) {
+
+	TRANSACTION_HOLDER.set(transaction);
+    }
+
+    /**
+     * Removes {@link UserTransaction} object from {@link ThreadLocal} per
+     * thread
+     */
+    public static void removeTransaction() {
+
+	TRANSACTION_HOLDER.remove();
+    }
+
+}
