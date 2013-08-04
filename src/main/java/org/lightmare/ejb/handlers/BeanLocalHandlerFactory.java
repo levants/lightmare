5f1ae2ef144bd14253f38b7c52411d1491b094d4
levan <levantsinadze@gmail.com>
2013-08-04 19:34:36 +0400

    improved BeanLocalHandlerFactory class

--- src/main/java/org/lightmare/ejb/handlers/BeanLocalHandlerFactory.java
+++ src/main/java/org/lightmare/ejb/handlers/BeanLocalHandlerFactory.java
@@ -4,11 +4,18 @@ import org.lightmare.remote.rpc.RPCall;
 
 public class BeanLocalHandlerFactory {
 
-    public static BeanLocalHandler get(Object... rpcArgs) {
+    private static RPCall getRPCall(Object... rpcArgs) {
 
 	String host = (String) rpcArgs[0];
 	int port = (Integer) rpcArgs[1];
 	RPCall call = new RPCall(host, port);
+
+	return call;
+    }
+
+    public static BeanLocalHandler get(Object... rpcArgs) {
+
+	RPCall call = getRPCall(rpcArgs);
 	BeanLocalHandler handler = new BeanLocalHandler(call);
 
 	return handler;
