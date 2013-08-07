fc87da9f97e9e1774a080fe35f28e2c96f1b936d
levan <levantsinadze@gmail.com>
2013-08-08 02:49:53 +0400

    added cloning performance testing for BeanHandler class

--- src/test/java/org/lightmare/ejb/handlers/BeanHandlerCloneTest.java
+++ src/test/java/org/lightmare/ejb/handlers/BeanHandlerCloneTest.java
@@ -1,5 +1,49 @@
 package org.lightmare.ejb.handlers;
 
+import org.junit.Test;
+import org.lightmare.bean.LightMareFalseBean;
+import org.lightmare.bean.LightMareFalseBeanRemote;
+import org.lightmare.cache.MetaData;
+import org.lightmare.utils.ObjectUtils;
+
 public class BeanHandlerCloneTest {
 
+    private static final int LIMIT = 100000;
+
+    @Test
+    public void handlerClonePerformanceTest() {
+
+	// LightMareFalseBean bean = new LightMareFalseBean();
+	MetaData metaData = new MetaData();
+	metaData.setBeanClass(LightMareFalseBean.class);
+	metaData.setInterfaceClasses(new Class<?>[] { LightMareFalseBeanRemote.class });
+
+	try {
+	    BeanHandler handler = new BeanHandler(metaData);
+	    long start = 0L;
+	    long current = 0L;
+	    long time = 0L;
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = new BeanHandler(metaData);
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.println(time);
+
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = (BeanHandler) handler.clone();
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.println(time);
+	    System.out.println("=========");
+	    System.out.println(ObjectUtils.notNull(handler));
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
+    }
 }
