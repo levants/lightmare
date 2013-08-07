81e8761314a87b8b662a9542d4e8750d232bd3e7
levan <levantsinadze@gmail.com>
2013-08-08 03:09:14 +0400

    added cloning performance testing for BeanHandler class

--- src/test/java/org/lightmare/ejb/handlers/BeanHandlerCloneTest.java
+++ src/test/java/org/lightmare/ejb/handlers/BeanHandlerCloneTest.java
@@ -13,14 +13,13 @@ import org.lightmare.utils.ObjectUtils;
 @RunWith(SortedRunner.class)
 public class BeanHandlerCloneTest {
 
-    private static final int LIMIT = 100000;
+    private static final int LIMIT = 1000000;
 
     private MetaData metaData;
 
     @Before
     public void configure() {
 
-	// LightMareFalseBean bean = new LightMareFalseBean();
 	metaData = new MetaData();
 	metaData.setBeanClass(LightMareFalseBean.class);
 	metaData.setInterfaceClasses(new Class<?>[] { LightMareFalseBeanRemote.class });
@@ -51,7 +50,7 @@ public class BeanHandlerCloneTest {
     }
 
     @Test
-    @RunOrder(1)
+    @RunOrder(2)
     public void newPerformanceTest() {
 
 	BeanHandler handler = new BeanHandler(metaData);
@@ -68,5 +67,104 @@ public class BeanHandlerCloneTest {
 	System.out.format("new %s\n", time);
 	System.out.println("=========");
 	System.out.println(ObjectUtils.notNull(handler));
+    }
+
+    @Test
+    @RunOrder(3)
+    public void newPerformanceTest2() {
+
+	BeanHandler handler = new BeanHandler(metaData);
+	long start = 0L;
+	long current = 0L;
+	long time = 0L;
+	start = System.currentTimeMillis();
+	for (int i = 0; i < LIMIT; i++) {
+
+	    handler = new BeanHandler(metaData);
+	}
+	current = System.currentTimeMillis();
+	time = current - start;
+	System.out.format("new2 %s\n", time);
+	System.out.println("=========");
+	System.out.println(ObjectUtils.notNull(handler));
+    }
+
+    @Test
+    @RunOrder(4)
+    public void handlerClonePerformanceTest2() {
+
+	try {
+	    LightMareFalseBean bean = new LightMareFalseBean();
+	    BeanHandler handler = new BeanHandler(metaData);
+	    long start = 0L;
+	    long current = 0L;
+	    long time = 0L;
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = BeanHandlerFactory.get(metaData, bean);
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.format("cloning2 %s\n", time);
+	    System.out.println("=========");
+	    System.out.println(ObjectUtils.notNull(handler));
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
+    }
+
+    @Test
+    @RunOrder(5)
+    public void handlerCloneNewPerformanceTest() {
+
+	try {
+	    System.out.println("=================");
+	    BeanHandler handler = new BeanHandler(metaData);
+	    long start = 0L;
+	    long current = 0L;
+	    long time = 0L;
+
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = new BeanHandler(metaData);
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.format("new %s\n", time);
+
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = (BeanHandler) handler.clone();
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.format("cloning %s\n", time);
+
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = (BeanHandler) handler.clone();
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.format("cloning %s\n", time);
+
+	    start = System.currentTimeMillis();
+	    for (int i = 0; i < LIMIT; i++) {
+
+		handler = new BeanHandler(metaData);
+	    }
+	    current = System.currentTimeMillis();
+	    time = current - start;
+	    System.out.format("new %s\n", time);
+
+	    System.out.println("=========");
+	    System.out.println(ObjectUtils.notNull(handler));
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
     }
 }
