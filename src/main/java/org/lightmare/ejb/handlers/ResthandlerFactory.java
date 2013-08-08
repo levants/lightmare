fb7024ad9c259575bc15c28a988ef52f1049fdce
levan <levantsinadze@gmail.com>
2013-08-08 20:45:39 +0400

    added comment to RestHandlerfactory class method

--- src/main/java/org/lightmare/ejb/handlers/ResthandlerFactory.java
+++ src/main/java/org/lightmare/ejb/handlers/ResthandlerFactory.java
@@ -8,6 +8,13 @@ package org.lightmare.ejb.handlers;
  */
 public class ResthandlerFactory {
 
+    /**
+     * Creates {@link RestHandler} instance
+     * 
+     * @param handler
+     * @param bean
+     * @return {@link RestHandler}
+     */
     public static <T> RestHandler<T> get(BeanHandler handler, T bean) {
 
 	RestHandler<T> restHandler = new RestHandler<T>(handler, bean);
