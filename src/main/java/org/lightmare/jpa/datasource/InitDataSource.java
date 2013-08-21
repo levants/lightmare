e138e534e14c54f3630cab4e0ab8f7edb8daec7f
levan <levantsinadze@gmail.com>
2013-08-21 23:44:28 +0400

    added abstract InitDataSource class

---
+++ src/main/java/org/lightmare/jpa/datasource/InitDataSource.java
@@ -0,0 +1,15 @@
+package org.lightmare.jpa.datasource;
+
+import javax.naming.Context;
+import javax.sql.DataSource;
+
+/**
+ * Initializes and bind to {@link Context} pooled {@link DataSource} object
+ * 
+ * @author levan
+ * 
+ */
+public abstract class InitDataSource {
+
+    
+}
