1fff8b45a2b996ece7b005600a00706fec0435cb
levan <levantsinadze@gmail.com>
2013-09-01 12:45:33 +0400

    added StringUtils.concat method

--- src/main/java/org/lightmare/utils/StringUtils.java
+++ src/main/java/org/lightmare/utils/StringUtils.java
@@ -3,4 +3,22 @@ package org.lightmare.utils;
 public class StringUtils {
 
     public static final String EMPTY_STRING = "";
+
+    public static String concat(Object... parts) {
+
+	String resultText;
+	if (ObjectUtils.available(parts)) {
+
+	    StringBuilder resultBuider = new StringBuilder();
+	    for (Object part : parts) {
+		resultBuider.append(part);
+	    }
+
+	    resultText = resultBuider.toString();
+	} else {
+	    resultText = null;
+	}
+
+	return resultText;
+    }
 }
