f6fc3d1bde56921dac69817c9da30ed5553a9fae
levan <levantsinadze@gmail.com>
2013-09-13 12:20:29 +0400

    edited Drivers enumerator class to DriverConfig class

--- src/main/java/org/lightmare/jpa/datasource/DriverConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/DriverConfig.java
@@ -45,7 +45,8 @@ public class DriverConfig {
 	ORACLE("oracle", "oracle.jdbc.OracleDriver"), MYSQL("mysql",
 		"com.mysql.jdbc.Driver"), MSSQL("mssql",
 		"com.microsoft.sqlserver.jdbc.SQLServerDriver"), DB2("db2",
-		"com.ibm.db2.jcc.DB2Driver"), H2("h2", "org.h2.Driver"), DERBY;
+		"com.ibm.db2.jcc.DB2Driver"), H2("h2", "org.h2.Driver"), DERBY(
+		"derby", "org.apache.derby.jdbc.EmbeddedDriver");
 
 	public String name;
 
