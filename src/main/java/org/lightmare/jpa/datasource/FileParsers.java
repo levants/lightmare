46b213d0955076d31ea53b285fcfcd83548ad7ce
levan <levantsinadze@gmail.com>
2013-09-13 12:01:36 +0400

    improved comment on FileParser class field

--- src/main/java/org/lightmare/jpa/datasource/FileParsers.java
+++ src/main/java/org/lightmare/jpa/datasource/FileParsers.java
@@ -37,6 +37,7 @@ import org.xml.sax.SAXException;
  */
 public class FileParsers {
 
+    // Tag names for file parser
     public static final String JBOSS_TAG_NAME = "urn:jboss:domain:datasources:1.0";
 
     private static final String DATA_SURCE_TAG = "datasource";
