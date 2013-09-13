e41f3a9e3b4a1a036e6576f8b8448fd88a59bebd
levan <levantsinadze@gmail.com>
2013-09-13 08:40:44 +0400

    edited comment on Configuration class field

--- src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
@@ -61,7 +61,7 @@ public class PoolConfig {
 	// Connection recovery properties
 	ACQUIRE_RETRY_ATTEMPTS("acquireRetryAttempts", "0"), // retry
 	ACQUIRE_RETRY_DELAY("acquireRetryDelay", "1000"), // delay
-	BREACK_AFTER_ACQUIRE_FAILURE("breakAfterAcquireFailure", "false");//
+	BREACK_AFTER_ACQUIRE_FAILURE("breakAfterAcquireFailure", "false");// breack
 
 	public String key;
 
