0586517f5e5429687f8038764fc3c6022173a420
levan <levantsinadze@gmail.com>
2013-09-13 08:17:29 +0400

    edited comment on Configuration class field

--- src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
@@ -47,7 +47,7 @@ public class PoolConfig {
 	// Pool timeout properties
 	MAX_IDLE_TIMEOUT("maxIdleTime","10000"),
 	MAX_IDLE_TIME_EXCESS_CONN("maxIdleTimeExcessConnections", "0"),
-	CHECK_OUT_TIMEOUT_NAME("checkoutTimeout","1800"),
+	CHECK_OUT_TIMEOUT("checkoutTimeout","1800"),
 	
 	// Controller properties
 	STAT_CACHE_NUM_DEFF_THREADS("statementCacheNumDeferredCloseThreads", "1"),
