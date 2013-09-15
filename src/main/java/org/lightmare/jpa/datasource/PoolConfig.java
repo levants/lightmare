4e3c7c0e1a32119f7fd68ea7c52c34a8686a07fd
levan <levantsinadze@gmail.com>
2013-09-15 20:56:47 +0400

    improved PoolConfig.Defaults enumeration class

--- src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
@@ -63,7 +63,7 @@ public class PoolConfig {
 	URESOLVED_TRANSACTIONS("forceIgnoreUnresolvedTransactions", "true"), // ignore
 
 	// Connection recovery properties
-	ACQUIRE_RETRY_ATTEMPTS("acquireRetryAttempts", "0"), // retry
+	ACQUIRE_RETRY_ATTEMPTS("acquireRetryAttempts", 0), // retry
 	ACQUIRE_RETRY_DELAY("acquireRetryDelay", "1000"), // delay
 	BREACK_AFTER_ACQUIRE_FAILURE("breakAfterAcquireFailure", "false");// break
 
