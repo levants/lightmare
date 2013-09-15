0b279277bcdf52965af226ed0a676d4535f21950
levan <levantsinadze@gmail.com>
2013-09-15 15:18:17 +0400

    improved Configuration utilities

--- src/test/java/org/lightmare/config/ConfigurationMergeTest.java
+++ src/test/java/org/lightmare/config/ConfigurationMergeTest.java
@@ -27,7 +27,7 @@ public class ConfigurationMergeTest {
 	subConfig1.put(Config.DATA_SOURCE_PATH.key, "path1");
 	subConfig1.put(Config.DEMPLOYMENT_PATH.key, "deployment1");
 	Map<Object, Object> subSubConfig1 = new HashMap<Object, Object>();
-	subSubConfig1.put(Configuration.PORT_KEY, "port_key1");
+	subSubConfig1.put(Config.PORT.key, "port_key1");
 	subSubConfig1.put(Configuration.BOSS_POOL_KEY, "boss_pool_key1");
 	subConfig1.put(subSubConfigKey, subSubConfig1);
 	config.put(subConfigKey, subConfig1);
