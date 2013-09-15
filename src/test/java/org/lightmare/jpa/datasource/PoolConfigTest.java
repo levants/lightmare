f443053f95545b9cbf04a640b0b4572855b33c49
levan <levantsinadze@gmail.com>
2013-09-15 21:01:07 +0400

    added test case for PoolConfig.Defaults enumeration class

---
+++ src/test/java/org/lightmare/jpa/datasource/PoolConfigTest.java
@@ -0,0 +1,16 @@
+package org.lightmare.jpa.datasource;
+
+import org.junit.Test;
+
+public class PoolConfigTest {
+
+    @Test
+    public void defaultsTest() {
+
+	System.out.format("%s %s\n", String.valueOf(true),
+		String.valueOf(Boolean.TRUE));
+
+	System.out.format("%s %s\n", String.valueOf(false),
+		String.valueOf(Boolean.FALSE));
+    }
+}
