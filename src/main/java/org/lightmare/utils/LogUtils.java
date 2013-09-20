e9525fb5f7aedbb1de33af6552dad9d25d340ae2
levan <levantsinadze@gmail.com>
2013-09-20 18:50:56 +0400

    improvements in ObjectUtils and CollectionUtils classes and all dependent classes

--- src/main/java/org/lightmare/utils/LogUtils.java
+++ src/main/java/org/lightmare/utils/LogUtils.java
@@ -13,7 +13,7 @@ public class LogUtils {
     public static String logMessage(String message, Object... formats) {
 
 	String logMessage;
-	if (ObjectUtils.available(formats)) {
+	if (CollectionUtils.available(formats)) {
 	    logMessage = String.format(message, formats);
 	} else {
 	    logMessage = message;
