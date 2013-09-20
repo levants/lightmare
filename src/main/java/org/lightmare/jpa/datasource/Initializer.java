119eb916cda684b33b8ed0af7fb37201564e24b4
levan <levantsinadze@gmail.com>
2013-09-20 19:11:41 +0400

    improvements in ObjectUtils and CollectionUtils classes and all dependent classes

--- src/main/java/org/lightmare/jpa/datasource/Initializer.java
+++ src/main/java/org/lightmare/jpa/datasource/Initializer.java
@@ -14,6 +14,7 @@ import javax.sql.DataSource;
 
 import org.lightmare.config.Configuration;
 import org.lightmare.jndi.JndiManager;
+import org.lightmare.utils.CollectionUtils;
 import org.lightmare.utils.ObjectUtils;
 import org.lightmare.utils.reflect.MetaUtils;
 
@@ -64,7 +65,7 @@ public class Initializer {
 
     private static boolean checkForDataSource(String path) {
 
-	return ObjectUtils.available(path);
+	return CollectionUtils.available(path);
     }
 
     public static String getJndiName(Properties properties) {
