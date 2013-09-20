3b59f45d2d2e350aaddcea80f4bbefafccb7e2e4
levan <levantsinadze@gmail.com>
2013-09-20 18:51:36 +0400

    improvements in ObjectUtils and CollectionUtils classes and all dependent classes

--- src/main/java/org/lightmare/utils/AbstractIOUtils.java
+++ src/main/java/org/lightmare/utils/AbstractIOUtils.java
@@ -367,7 +367,7 @@ public abstract class AbstractIOUtils {
 
 	File parentFile;
 
-	if (ObjectUtils.available(files)) {
+	if (CollectionUtils.available(files)) {
 	    parentFile = CollectionUtils.getFirst(files);
 	} else {
 	    parentFile = realFile;
