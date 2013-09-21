7cf363916d7a22c947703917bc9415f7e4d4afa8
levan <levantsinadze@gmail.com>
2013-09-21 16:41:03 +0400

    improved locking utilities

--- src/main/java/org/lightmare/deploy/LoaderPoolManager.java
+++ src/main/java/org/lightmare/deploy/LoaderPoolManager.java
@@ -167,7 +167,8 @@ public class LoaderPoolManager {
 	    boolean locked = Boolean.FALSE;
 	    while (ObjectUtils.notTrue(locked)) {
 
-		// Locks the Lock object to avoid shut down in parallel
+		// Locks the Lock object to avoid shut down and submit in
+		// parallel
 		locked = ObjectUtils.tryLock(LOCK, LOCK_TIME,
 			TimeUnit.MILLISECONDS);
 		if (locked) {
