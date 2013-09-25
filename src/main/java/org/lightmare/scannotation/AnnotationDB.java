dc3f19240595672ac22adc199b6313b9f455eed0
levan <levantsinadze@gmail.com>
2013-09-25 16:19:05 +0400

    improved references for cached classes

--- src/main/java/org/lightmare/scannotation/AnnotationDB.java
+++ src/main/java/org/lightmare/scannotation/AnnotationDB.java
@@ -5,7 +5,6 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
