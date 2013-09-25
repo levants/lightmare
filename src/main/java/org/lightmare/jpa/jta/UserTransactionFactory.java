417ccbf1aeb1ce01846cff926c5dfec642963455
levan <levantsinadze@gmail.com>
2013-09-25 16:14:21 +0400

    added factory class for UserTrnsaction initialization

---
+++ src/main/java/org/lightmare/jpa/jta/UserTransactionFactory.java
@@ -0,0 +1,13 @@
+package org.lightmare.jpa.jta;
+
+import javax.transaction.UserTransaction;
+
+/**
+ * Factory class to initialize and create {@link UserTransaction} instance
+ * 
+ * @author levan
+ * 
+ */
+public class UserTransactionFactory {
+
+}
