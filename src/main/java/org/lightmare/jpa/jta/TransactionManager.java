2e08764b78910dac378d8bd2163175a180a2d36e
levan <levantsinadze@gmail.com>
2013-09-26 17:32:04 +0400

    improved JTA transaction utilities

--- src/main/java/org/lightmare/jpa/jta/TransactionManager.java
+++ src/main/java/org/lightmare/jpa/jta/TransactionManager.java
@@ -10,4 +10,115 @@ import javax.transaction.UserTransaction;
  */
 public class TransactionManager {
 
+    
+    protected static void addEntityTransaction(UserTransaction userTransaction,
+	    EntityTransaction entityTransaction, EntityManager em) {
+
+	if (userTransaction instanceof UserTransactionImpl) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+
+	    if (ObjectUtils.notNull(entityTransaction)) {
+		transaction.addTransaction(entityTransaction);
+	    }
+
+	    if (ObjectUtils.notNull(em)) {
+		transaction.addEntityManager(em);
+	    }
+
+	}
+    }
+
+    protected static void addEntityTransactions(
+	    UserTransaction userTransaction,
+	    Collection<BeanTransactions.TransactionData> entityTransactions) {
+
+	if (userTransaction instanceof UserTransactionImpl
+		&& CollectionUtils.valid(entityTransactions)) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+
+	    for (BeanTransactions.TransactionData transactionData : entityTransactions) {
+		addEntityTransaction(transaction,
+			transactionData.entityTransaction, transactionData.em);
+	    }
+	}
+    }
+
+    protected static void addEntityManager(UserTransaction userTransaction,
+	    EntityManager em) {
+
+	if (userTransaction instanceof UserTransactionImpl
+		&& ObjectUtils.notNull(em)) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+	    transaction.addEntityManager(em);
+	}
+    }
+
+    private static void addEntityManagers(UserTransaction userTransaction,
+	    Collection<EntityManager> ems) {
+
+	if (userTransaction instanceof UserTransactionImpl
+		&& CollectionUtils.valid(ems)) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+	    for (EntityManager em : ems) {
+		addEntityManager(transaction, em);
+	    }
+	}
+    }
+
+    private static void addReqNewTransaction(UserTransaction userTransaction,
+	    EntityTransaction entityTransaction, EntityManager em) {
+
+	if (userTransaction instanceof UserTransactionImpl) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+
+	    if (ObjectUtils.notNull(entityTransaction)) {
+		transaction.pushReqNew(entityTransaction);
+	    }
+
+	    if (ObjectUtils.notNull(em)) {
+		transaction.pushReqNewEm(em);
+	    }
+	}
+    }
+
+    private static void addReqNewTransactions(UserTransaction userTransaction,
+	    Collection<BeanTransactions.TransactionData> entityTransactions) {
+
+	if (userTransaction instanceof UserTransactionImpl
+		&& CollectionUtils.valid(entityTransactions)) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+
+	    for (BeanTransactions.TransactionData transactionData : entityTransactions) {
+		addReqNewTransaction(transaction,
+			transactionData.entityTransaction, transactionData.em);
+	    }
+	}
+    }
+
+    private static void addCaller(UserTransaction userTransaction,
+	    BeanHandler handler) {
+
+	if (userTransaction instanceof UserTransactionImpl) {
+
+	    UserTransactionImpl transaction = ObjectUtils.cast(userTransaction,
+		    UserTransactionImpl.class);
+
+	    Object caller = transaction.getCaller();
+	    if (caller == null) {
+		transaction.setCaller(handler);
+	    }
+	}
+    }
 }
