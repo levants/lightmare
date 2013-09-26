7c6356e67bb9ec0eedd4db0b92de7918cf912a12
levan <levantsinadze@gmail.com>
2013-09-26 17:31:36 +0400

    improved JTA transaction utilities

---
+++ src/main/java/org/lightmare/jpa/jta/TransactionManager.java
@@ -0,0 +1,13 @@
+package org.lightmare.jpa.jta;
+
+import javax.transaction.UserTransaction;
+
+/**
+ * Class to manager {@link UserTransaction} creation and closure
+ * 
+ * @author levan
+ * 
+ */
+public class TransactionManager {
+
+}
