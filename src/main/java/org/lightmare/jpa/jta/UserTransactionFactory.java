41b1e76588667b3b0777d43e86175a9647c44bdc
levan <levantsinadze@gmail.com>
2013-09-26 18:08:04 +0400

    improved JTA transaction utilities

--- src/main/java/org/lightmare/jpa/jta/UserTransactionFactory.java
+++ src/main/java/org/lightmare/jpa/jta/UserTransactionFactory.java
@@ -14,6 +14,7 @@ import org.lightmare.utils.ObjectUtils;
  */
 public abstract class UserTransactionFactory {
 
+    // Inacrive state of user transaction
     public static final int INSACTIVE_TRANSACTION_STATUS = 0;
 
     // Error messages
