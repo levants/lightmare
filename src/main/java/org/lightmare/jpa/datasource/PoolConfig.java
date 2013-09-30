7c48b76cd56a991856c7f4b9ddeb164562531d90
levan <levantsinadze@gmail.com>
2013-09-30 15:05:44 +0400

    improved connection and / or JTA transaction utilities

--- src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/PoolConfig.java
@@ -335,6 +335,7 @@ public class PoolConfig {
 	this.poolProviderType = poolProviderType;
     }
 
+    // Sets appropriate connection pool provider
     public void setPoolProviderType(String poolProviderTypeName) {
 
 	PoolProviderType[] types = PoolProviderType.values();
