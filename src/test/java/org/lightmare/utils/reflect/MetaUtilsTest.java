1626b17147cbe535fd01cb0008c83300a36cad03
levan <levantsinadze@gmail.com>
2013-10-01 16:55:56 +0400

    added test cases

--- src/test/java/org/lightmare/utils/reflect/MetaUtilsTest.java
+++ src/test/java/org/lightmare/utils/reflect/MetaUtilsTest.java
@@ -1,8 +1,12 @@
 package org.lightmare.utils.reflect;
 
+import org.junit.Test;
+
 public class MetaUtilsTest {
 
+    @Test
     public void testDefaultValues() {
 
+	System.out.println(MetaUtils.getDefault(Byte.class));
     }
 }
