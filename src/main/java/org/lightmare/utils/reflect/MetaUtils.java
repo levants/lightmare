8c89bf6193427e33d9c511ca0d500400b8e28f63
levan <levantsinadze@gmail.com>
2013-10-01 19:07:28 +0400

    improved reflection utilities

--- src/main/java/org/lightmare/utils/reflect/MetaUtils.java
+++ src/main/java/org/lightmare/utils/reflect/MetaUtils.java
@@ -100,11 +100,6 @@ public class MetaUtils {
 	    } finally {
 		ObjectUtils.unlock(ACCESSOR_LOCK);
 	    }
-
-	    boolean locked = Boolean.FALSE;
-	    while (ObjectUtils.notTrue(locked)) {
-		locked = makeAccessible(accessibleObject);
-	    }
 	}
     }
 
