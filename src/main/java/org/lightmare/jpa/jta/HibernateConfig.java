a600d5e9434d666b448ea5dab1fec1bbaaffa794
levan <levantsinadze@gmail.com>
2013-10-02 18:02:50 +0400

    improved pool configuration utilities

--- src/main/java/org/lightmare/jpa/jta/HibernateConfig.java
+++ src/main/java/org/lightmare/jpa/jta/HibernateConfig.java
@@ -8,14 +8,13 @@ package org.lightmare.jpa.jta;
  */
 public enum HibernateConfig {
 
-    // JTA configuration for Hibernate deployment
+    // JTA configuration for Hibernate deployment - JTA platform
     PLATFORM("hibernate.transaction.jta.platform",
-	    "org.hibernate.service.jta.platform.internal.BitronixJtaPlatform"), // JTA
-										// platform
+	    "org.hibernate.service.jta.platform.internal.BitronixJtaPlatform"),
 
+    // factory class
     FACTORY("hibernate.transaction.factory_class",
-	    "org.hibernate.engine.transaction.internal.jta.JtaTransactionFactory"); // factory
-										    // class
+	    "org.hibernate.engine.transaction.internal.jta.JtaTransactionFactory");
 
     public String key;
 
