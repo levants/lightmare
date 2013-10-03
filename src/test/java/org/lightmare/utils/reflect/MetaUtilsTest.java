6612cdc388196dd638365d0482793f1812573c6d
levan <levantsinadze@gmail.com>
2013-10-03 15:50:32 +0400

    added test cases

--- src/test/java/org/lightmare/utils/reflect/MetaUtilsTest.java
+++ src/test/java/org/lightmare/utils/reflect/MetaUtilsTest.java
@@ -3,11 +3,16 @@ package org.lightmare.utils.reflect;
 import java.io.IOException;
 import java.lang.reflect.AccessibleObject;
 
+import javax.persistence.EntityManager;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.lightmare.bean.LightMareBean;
+import org.lightmare.utils.ObjectUtils;
 
 public class MetaUtilsTest {
+
+    protected EntityManager em;
 
     @Test
     public void testDefaultValues() {
@@ -31,9 +36,15 @@ public class MetaUtilsTest {
 	    AccessibleObject acc2 = MetaUtils.getDeclaredField(
 		    LightMareBean.class, "em");
 
+	    AccessibleObject acc3 = MetaUtils.getDeclaredField(
+		    MetaUtilsTest.class, "em");
+
 	    Assert.assertTrue("AccessibleObject instances are not the same",
 		    acc1.equals(acc2));
 	    System.out.format("%s %s", acc1, acc2);
+	    Assert.assertTrue("AccessibleObject instances are not the same",
+		    ObjectUtils.notEquals(acc1, acc3));
+	    System.out.format("%s %s", acc1, acc3);
 
 	} catch (IOException ex) {
 	    ex.printStackTrace();
