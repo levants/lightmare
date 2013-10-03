1662c92c66808ea3a7ab2de986c51dc1d13d3966
levan <levantsinadze@gmail.com>
2013-10-03 16:46:46 +0400

    improved / commented utility classes code added test cases

--- src/main/java/org/lightmare/jpa/datasource/DriverConfig.java
+++ src/main/java/org/lightmare/jpa/datasource/DriverConfig.java
@@ -49,7 +49,7 @@ public abstract class DriverConfig {
      */
     public static String getDriverName(String name) {
 
-	String driverName;
+	String driverName = null;
 
 	Drivers[] drivers = Drivers.values();
 	Drivers driver;
@@ -67,8 +67,6 @@ public abstract class DriverConfig {
 	if (length == 0 || ObjectUtils.notTrue(match)) {
 	    driverName = null;
 	}
-
-
 
 	return driverName;
     }
