90c892ec22a7cde7d8d7d6a3d8943b769cc0ebf1
levan <levantsinadze@gmail.com>
2013-10-04 12:32:01 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/rest/providers/ApplicationInit.java
+++ src/main/java/org/lightmare/rest/providers/ApplicationInit.java
@@ -9,26 +9,20 @@ import org.lightmare.rest.RestConfig;
  * 
  */
 public enum ApplicationInit {
-    
-    APPLICATION_INIT("javax.ws.rs.Application", RestConfig.class
-	    .getName()),
 
-    public static final String REST_DEFAULT_URI = "/rest";
+    APPLICATION_INIT("javax.ws.rs.Application", RestConfig.class.getName()),
 
-    public static final String APPLICATION_INIT_PARAM_NAME = "javax.ws.rs.Application";
+    REST_DEFAULT_URI("/rest");
 
-    public static final String APPLICATION_INIT_PARAM = RestConfig.class
-	    .getName();
-    
     public String key;
-    
+
     public String value;
-    
-    private ApplicationInit(String key){
+
+    private ApplicationInit(String key) {
 	this.key = key;
     }
-    
-    private ApplicationInit(String key, String value){
+
+    private ApplicationInit(String key, String value) {
 	this(key);
 	this.value = value;
     }
