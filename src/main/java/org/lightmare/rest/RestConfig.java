95d0b90362d9d6261f1b289cd01ed69e67f7a6e2
levan <levantsinadze@gmail.com>
2013-10-04 12:26:11 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/rest/RestConfig.java
+++ src/main/java/org/lightmare/rest/RestConfig.java
@@ -23,7 +23,7 @@ import org.lightmare.utils.ObjectUtils;
  * class to add and remove {@link Resource}'s and reload at runtime
  * 
  * @author levan
- * 
+ * @since 0.0.50-SNAPSHOT
  */
 public class RestConfig extends ResourceConfig {
 
