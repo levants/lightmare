5999017295f46aef64d002e01d595380db4fbef1
levan <levantsinadze@gmail.com>
2013-10-04 23:58:34 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/ejb/handlers/BeanHandler.java
+++ src/main/java/org/lightmare/ejb/handlers/BeanHandler.java
@@ -3,7 +3,6 @@ package org.lightmare.ejb.handlers;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
