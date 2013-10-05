dc042b01074f164b6e47fee4e669f093437df2cd
levan <levantsinadze@gmail.com>
2013-10-06 00:05:21 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/utils/CollectionUtils.java
+++ src/main/java/org/lightmare/utils/CollectionUtils.java
@@ -25,6 +25,7 @@ public abstract class CollectionUtils {
     // Second index of array
     public static final int SECOND_INDEX = 1;
 
+    // Index of not existing data in collection
     public static final int NOT_EXISTING_INDEX = -1;
 
     public static final int EMPTY_ARRAY_LENGTH = 0;
