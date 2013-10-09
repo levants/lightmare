49c05f44285996213b99644940cfbe23f46d92ea
levan <levantsinadze@gmail.com>
2013-10-09 14:18:32 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/config/Configuration.java
+++ src/main/java/org/lightmare/config/Configuration.java
@@ -300,7 +300,7 @@ public class Configuration implements Cloneable {
 
 	setIfContains(ConfigKeys.PORT.key, ConfigKeys.PORT.value);
 
-	    setIfContains(ConfigKeys.BOSS_POOL.key, ConfigKeys.BOSS_POOL.value);
+	setIfContains(ConfigKeys.BOSS_POOL.key, ConfigKeys.BOSS_POOL.value);
 
 	contains = containsConfigKey(ConfigKeys.WORKER_POOL.key);
 	if (ObjectUtils.notTrue(contains)) {
