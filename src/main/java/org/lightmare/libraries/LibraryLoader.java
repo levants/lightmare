530c0642564ab2b0b213d53ae9525a1ccc2b83e4
levan <levantsinadze@gmail.com>
2013-10-10 18:54:13 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/libraries/LibraryLoader.java
+++ src/main/java/org/lightmare/libraries/LibraryLoader.java
@@ -78,6 +78,17 @@ public class LibraryLoader {
 	    return loader;
 	}
     }
+    
+    private static void initURLMethod() throws IOException{
+	if (addURLMethod == null
+		&& MetaUtils.hasMethod(URLClassLoader.class,
+			ADD_URL_METHOD_NAME)) {
+	    addURLMethod = MetaUtils.getDeclaredMethod(
+		    URLClassLoader.class, ADD_URL_METHOD_NAME,
+		    URL.class);
+	}
+    }
+    
 
     /**
      * Gets {@link URLClassLoader} class addURL method
