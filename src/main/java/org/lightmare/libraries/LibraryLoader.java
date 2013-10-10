3c964b2587f30b54bf15301364e840404b995649
levan <levantsinadze@gmail.com>
2013-10-10 18:49:32 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/libraries/LibraryLoader.java
+++ src/main/java/org/lightmare/libraries/LibraryLoader.java
@@ -42,7 +42,7 @@ public class LibraryLoader {
     // Name of class loader using thread
     private static final String LOADER_THREAD_NAME = "library-class-loader-thread";
 
-    //Inaccessible method to add URL to existing class loader
+    // Inaccessible method to add URL to existing class loader
     private static Method addURLMethod;
 
     private static final Lock LOCK = new ReentrantLock();
