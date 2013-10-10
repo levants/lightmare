d9d11d5c18eddc774e70ebdef8305650d588ea72
levan <levantsinadze@gmail.com>
2013-10-10 14:52:38 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/cache/ConnectionContainer.java
+++ src/main/java/org/lightmare/cache/ConnectionContainer.java
@@ -22,7 +22,7 @@ import org.lightmare.utils.StringUtils;
  * Container class to cache connections and connection types
  * 
  * @author levan
- * 
+ * @since 0.0.65-SNAPSHOT
  */
 public class ConnectionContainer {
 
