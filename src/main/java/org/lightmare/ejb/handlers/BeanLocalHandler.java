850995a93b3f3d72dbcf35675f06bb214e3ba299
levan <levantsinadze@gmail.com>
2013-10-10 18:41:03 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/ejb/handlers/BeanLocalHandler.java
+++ src/main/java/org/lightmare/ejb/handlers/BeanLocalHandler.java
@@ -14,6 +14,7 @@ import org.lightmare.utils.RpcUtils;
  */
 public class BeanLocalHandler implements InvocationHandler {
 
+    //Instance of RPC caller
     private RPCall rpCall;
 
     protected BeanLocalHandler(RPCall rpCall) {
