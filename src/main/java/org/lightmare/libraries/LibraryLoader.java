8d7619dd447016652018f601e56c811cc9787813
levan <levantsinadze@gmail.com>
2013-10-10 21:34:03 +0400

    improved code / comments at utility classes

--- src/main/java/org/lightmare/libraries/LibraryLoader.java
+++ src/main/java/org/lightmare/libraries/LibraryLoader.java
@@ -408,6 +408,19 @@ public class LibraryLoader {
 	ClassLoader loader = getContextClassLoader();
 	loadClasses(classes, loader);
     }
+    
+    private static void hasCloseMethod(ClassLoader loader) throws IOException{
+	if (hasCloseMethod == null) {
+	    // Finds if loader associated class or superclass has
+	    // "close"
+	    // method
+	    Class<? extends ClassLoader> loaderClass = loader
+		    .getClass();
+	    boolean hasMethod = MetaUtils.hasPublicMethod(loaderClass,
+		    CLOSE_METHOD_NAME);
+	    hasCloseMethod = hasMethod;
+	}
+    }
 
     /**
      * Checks and caches if passed {@link ClassLoader} implementation or it's
