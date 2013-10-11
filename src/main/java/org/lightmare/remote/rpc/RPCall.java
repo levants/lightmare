59b604eb603612f0ec8537e6614c95290015b67d
levan <levantsinadze@gmail.com>
2013-10-11 16:51:05 +0400

    improved / commented code in utility classes

--- src/main/java/org/lightmare/remote/rpc/RPCall.java
+++ src/main/java/org/lightmare/remote/rpc/RPCall.java
@@ -31,7 +31,7 @@ import org.lightmare.utils.concurrent.ThreadFactoryUtil;
  * Client class to produce remote procedure call
  * 
  * @author levan
- * 
+ * @since 0.0.21-SNAPSHOT
  */
 public class RPCall {
 
