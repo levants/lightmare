f43f7f6322918013b8c65378ded5d13637c7e61f
levan <levantsinadze@gmail.com>
2013-10-17 01:44:02 +0400

    improved / commented code utiliti classes

--- src/main/java/org/lightmare/jndi/LightmareContext.java
+++ src/main/java/org/lightmare/jndi/LightmareContext.java
@@ -93,7 +93,6 @@ public class LightmareContext extends MemoryContext {
 	    } catch (IOException ex) {
 		throw new NamingException(ex.getMessage());
 	    }
-
 	} else {
 	    value = super.lookup(jndiName);
 	}
