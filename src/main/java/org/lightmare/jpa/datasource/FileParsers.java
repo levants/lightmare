4140c1873092baaf03356cbc1f51c6f6b88813a3
levan <levantsinadze@gmail.com>
2013-10-17 02:48:56 +0400

    improved / commented code utiliti classes

--- src/main/java/org/lightmare/jpa/datasource/FileParsers.java
+++ src/main/java/org/lightmare/jpa/datasource/FileParsers.java
@@ -113,7 +113,8 @@ public class FileParsers {
      */
     public static Document parse(InputStream stream) throws IOException {
 Document document;
-	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	
+DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	DocumentBuilder builder;
 	
 
