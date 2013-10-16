241b39f1118689fed7036fb33435ac7ce04d56f1
levan <levantsinadze@gmail.com>
2013-10-17 03:13:49 +0400

    improved / commented code utiliti classes

--- src/main/java/org/lightmare/config/Configuration.java
+++ src/main/java/org/lightmare/config/Configuration.java
@@ -312,7 +312,6 @@ public class Configuration implements Cloneable {
 
 	// Sets default values to remote server configuration
 	setIfContains(ConfigKeys.IP_ADDRESS.key, ConfigKeys.IP_ADDRESS.value);
-
 	setIfContains(ConfigKeys.PORT.key, ConfigKeys.PORT.value);
 
 	setIfContains(ConfigKeys.BOSS_POOL.key, ConfigKeys.BOSS_POOL.value);
