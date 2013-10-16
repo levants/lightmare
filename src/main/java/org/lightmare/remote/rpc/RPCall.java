6eecb3ee5ef50969c161dd704787542b7fd3a584
levan <levantsinadze@gmail.com>
2013-10-17 01:28:08 +0400

    improved / commented code utiliti classes

--- src/main/java/org/lightmare/remote/rpc/RPCall.java
+++ src/main/java/org/lightmare/remote/rpc/RPCall.java
@@ -71,6 +71,10 @@ public class RPCall {
 	this.port = port;
     }
 
+    /**
+     * Configures RPC service client
+     * @param config
+     */
     public static void configure(Configuration config) {
 
 	if (worker == null) {
