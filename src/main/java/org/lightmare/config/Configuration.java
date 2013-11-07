74b28f59d2e60cb11ae66e97853605c1cf80ba7f
levan <levantsinadze@gmail.com>
2013-11-07 21:01:18 +0400

    improved / commented code in utility classes

--- src/main/java/org/lightmare/config/Configuration.java
+++ src/main/java/org/lightmare/config/Configuration.java
@@ -184,7 +184,8 @@ public class Configuration implements Cloneable {
 
     /**
      * Gets value from sub configuration for passed sub key contained in
-     * configuration for default configuration key and if such not exists returns passed devault value
+     * configuration for default configuration key and if such not exists
+     * returns passed devault value
      * 
      * @param subKey
      * @param defaultValue
