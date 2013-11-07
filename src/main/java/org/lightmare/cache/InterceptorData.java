09d8feb26a2cbb490b51b1832636c717e88a3043
levan <levantsinadze@gmail.com>
2013-11-07 20:18:32 +0400

    improved / commented code in utility classes

--- src/main/java/org/lightmare/cache/InterceptorData.java
+++ src/main/java/org/lightmare/cache/InterceptorData.java
@@ -11,7 +11,7 @@ import java.lang.reflect.Method;
  */
 public class InterceptorData {
 
-    // Injection bean class
+    // Annotated EJB bean class
     private Class<?> BeanClass;
 
     private Method beanMethod;
