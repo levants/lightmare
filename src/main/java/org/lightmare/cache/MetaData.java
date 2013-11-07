0acacfcbe37bcfb4dfb68f1bf46db20b99a30fce
levan <levantsinadze@gmail.com>
2013-11-07 20:39:07 +0400

    improved / commented code in utility classes

--- src/main/java/org/lightmare/cache/MetaData.java
+++ src/main/java/org/lightmare/cache/MetaData.java
@@ -42,7 +42,7 @@ public class MetaData {
     // All EJB remote interfaces
     private Class<?>[] remoteInterfaces;
 
-    //EJB bean's Field to set JTA UserTransaction instance
+    // EJB bean's Field to set JTA UserTransaction instance
     private Field transactionField;
 
     private Collection<ConnectionData> connections;
