2b88ddf95135aa1805b5d559e2b23b63e3410585
levan <levantsinadze@gmail.com>
2013-12-12 18:10:40 +0400

    added comments to utility classes

--- src/main/java/org/lightmare/utils/Cleanable.java
+++ src/main/java/org/lightmare/utils/Cleanable.java
@@ -2,6 +2,12 @@ package org.lightmare.utils;
 
 import java.io.IOException;
 
+/**
+ * Interface which should be implemented for phantom reference
+ * 
+ * @author levan
+ * @see CleanUtils
+ */
 public interface Cleanable {
 
     void clean() throws IOException;
