267ab1e4062aa1d0ca5e49180787c8c63b215344
levan <levantsinadze@gmail.com>
2013-12-12 18:11:01 +0400

    added comments to utility classes

--- src/main/java/org/lightmare/utils/Cleanable.java
+++ src/main/java/org/lightmare/utils/Cleanable.java
@@ -3,7 +3,8 @@ package org.lightmare.utils;
 import java.io.IOException;
 
 /**
- * Interface which should be implemented for phantom reference
+ * Interface which should be implemented for phantom reference to close unused
+ * resouyrces
  * 
  * @author levan
  * @see CleanUtils
