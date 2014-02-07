d2c28f46313c500cbc6b2758e0c676b0519095f1
levan <levantsinadze@gmail.com>
2014-02-08 02:09:52 +0400

    improved code

--- src/main/java/org/lightmare/cache/RestContainer.java
+++ src/main/java/org/lightmare/cache/RestContainer.java
@@ -1,3 +1,25 @@
+/*
+ * Lightmare, Embeddable ejb container (works for stateless session beans) with JPA / Hibernate support
+ *
+ * Copyright (c) 2013, Levan Tsinadze, or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.lightmare.cache;
 
 import java.util.Collection;
