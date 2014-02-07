c4840d95892add975bac55cd69bf9b0b6b3d35bf
levan <levantsinadze@gmail.com>
2014-02-08 03:43:25 +0400

    improved code

--- src/test/java/org/lightmare/utils/fs/codecs/JarUtilsTest.java
+++ src/test/java/org/lightmare/utils/fs/codecs/JarUtilsTest.java
@@ -1,13 +1,20 @@
 package org.lightmare.utils.fs.codecs;
 
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.net.URI;
 import java.net.URL;
 import java.util.zip.ZipFile;
+
+import org.junit.Test;
 
 public class JarUtilsTest {
 
     public static String URI_TEXT = "./lib/loader-tester.jar";
 
+    public static byte[] MAGIC = { 'P', 'K', 0x3, 0x4 };
+
+    @Test
     public void checkUriTest() {
 
 	try {
@@ -15,8 +22,24 @@ public class JarUtilsTest {
 	    URI uri = url.toURI();
 	    String path = uri.getSchemeSpecificPart();
 	    System.out.println(path);
-	    ZipFile zipFile = new ZipFile(path);
-	    zipFile.close();
+
+	    File file = new File(path);
+	    boolean isZip = true;
+	    byte[] buffer = new byte[MAGIC.length];
+	    try {
+		RandomAccessFile raf = new RandomAccessFile(file, "r");
+		raf.readFully(buffer);
+		for (int i = 0; i < MAGIC.length; i++) {
+		    if (buffer[i] != MAGIC[i]) {
+			isZip = false;
+			break;
+		    }
+		}
+		raf.close();
+	    } catch (Throwable e) {
+		isZip = false;
+	    }
+	    System.out.println(isZip);
 	} catch (Exception ex) {
 	    ex.printStackTrace();
 	}
