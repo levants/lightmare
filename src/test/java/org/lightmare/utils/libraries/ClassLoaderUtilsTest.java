b8f7c43998c7f315b9af7980e0fbd6e59e8b90d5
levan <levantsinadze@gmail.com>
2014-02-10 18:51:12 +0400

    improved test cases

--- src/test/java/org/lightmare/utils/libraries/ClassLoaderUtilsTest.java
+++ src/test/java/org/lightmare/utils/libraries/ClassLoaderUtilsTest.java
@@ -7,9 +7,33 @@ public class ClassLoaderUtilsTest {
     @Test
     public void javaVedotCheckTest() {
 
-	System.out.println(System.getProperty("java.vendor"));
-	System.out.println(System.getProperty("java.vendor.url"));
-	System.out.println(System.getProperty("java.version"));
-	System.out.println(System.getProperty("java.vm.vendor"));
+	try {
+	    System.out.println();
+	    System.out
+		    .println("==========JVM and Java Platform properties==================");
+	    System.out.println();
+	    System.out.println("=========================================");
+	    System.out.println("=========================================");
+
+	    System.out.println(System.getProperty("java.vendor"));
+	    System.out.println(System.getProperty("java.vendor.url"));
+	    System.out.println(System.getProperty("java.version"));
+	    System.out.println(System.getProperty("java.vm.vendor"));
+
+	    System.out.println("=========================================");
+	    System.out.println("=========================================");
+	    System.out.println("=========================================");
+
+	    System.out.println(System.getProperty("os.arch"));
+
+	    System.out.println();
+	    System.out
+		    .println("==========JVM and Java Platform properties==================");
+	    System.out.println();
+	    System.out.println("=========================================");
+	    System.out.println();
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
     }
 }
