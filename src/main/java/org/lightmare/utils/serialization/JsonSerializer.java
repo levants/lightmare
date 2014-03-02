a863aed0dcf02951a2349a7f5253e54f5aad525b
levan <levantsinadze@gmail.com>
2014-03-02 15:55:28 +0400

    upgraded dependencies in pom.xml file and improved code in utility classes and edited ignore file

--- src/main/java/org/lightmare/utils/serialization/JsonSerializer.java
+++ src/main/java/org/lightmare/utils/serialization/JsonSerializer.java
@@ -1,17 +1,38 @@
+/*
+ * Lightmare, Embeddable ejb container (works for stateless session beans) with JPA / Hibernate support
+ *
+ * Copyright (c) 2013, Levan Tsinadze, or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.lightmare.utils.serialization;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-import org.lightmare.utils.ObjectUtils;
 import org.lightmare.utils.RpcUtils;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 /**
  * Reads write java objects from {@link Byte} array or {@link String} with java
@@ -22,25 +43,24 @@ import com.fasterxml.jackson.databind.SerializationFeature;
  */
 public abstract class JsonSerializer {
 
-    // Converter class
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    /**
+     * Enumeration to initialize and configure single {@link ObjectMapper}
+     * instance
+     * 
+     * @author Levan Tsinadze
+     * @since o.1.1
+     */
+    protected static enum JsonMapper {
 
-    // Check for configuration of ObjectMapper instance
-    private static boolean mapperConfigured;
+	MAPPER; // Single instance of converter container class
 
-    // Lock for initializing ObjectMapper instance
-    private static final Lock LOCK = new ReentrantLock();
+	// Converter class
+	protected final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static void configureMapper() {
-
-	ObjectUtils.lock(LOCK);
-	try {
-	    if (ObjectUtils.notTrue(mapperConfigured)) {
-		MAPPER.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		mapperConfigured = Boolean.TRUE;
-	    }
-	} finally {
-	    ObjectUtils.unlock(LOCK);
+	private JsonMapper() {
+	    objectMapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+		    .registerModule(new AfterburnerModule())
+		    .registerModule(new Hibernate4Module());
 	}
     }
 
@@ -51,17 +71,12 @@ public abstract class JsonSerializer {
      * @return {@link ObjectMapper}
      */
     public static ObjectMapper getMapper() {
-
-	if (ObjectUtils.notTrue(mapperConfigured)) {
-	    configureMapper();
-	}
-
-	return MAPPER;
+	return JsonMapper.MAPPER.objectMapper;
     }
 
     /**
      * Serializes {@link Object} to JSON {@link String} with <a
-     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
+     * href="https://github.com/FasterXML/jackson-databind">jackson API</a>
      * 
      * @param value
      * @return {@link String}
@@ -85,7 +100,7 @@ public abstract class JsonSerializer {
     }
 
     /**
-     * De-serializes JSON {@link String} to {@link Object} with <a
+     * For deserializatio of JSON {@link String} to {@link Object} with <a
      * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
      * 
      * @param data
@@ -112,8 +127,8 @@ public abstract class JsonSerializer {
     }
 
     /**
-     * Deserializes JSON {@link InputStream} to {@link Object} with <a
-     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
+     * For deserializatio of JSON {@link InputStream} to {@link Object} with <a
+     * href="https://github.com/FasterXML/jackson-databind">jackson API</a>
      * 
      * @param data
      * @param valueClass
@@ -129,8 +144,8 @@ public abstract class JsonSerializer {
     }
 
     /**
-     * Deserializes JSON (<code>byte[]</code>) to {@link Object} with <a
-     * href="https://github.com/FasterXML/jackson-databind">jackson api</a>
+     * For deserializatio of JSON (<code>byte[]</code>) to {@link Object} with
+     * <a href="https://github.com/FasterXML/jackson-databind">jackson API</a>
      * 
      * @param data
      * @param valueClass
