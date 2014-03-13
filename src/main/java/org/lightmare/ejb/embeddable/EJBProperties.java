117b3366ea0195f757debdc90b047d6f527bda48
levan <levantsinadze@gmail.com>
2014-03-13 10:12:15 +0400

    improved embeddable EJB container utility classes

--- src/main/java/org/lightmare/ejb/embeddable/EJBProperties.java
+++ src/main/java/org/lightmare/ejb/embeddable/EJBProperties.java
@@ -7,17 +7,37 @@ import javax.ejb.embeddable.EJBContainer;
 
 import org.lightmare.utils.ObjectUtils;
 
-public class EjbProperties {
+/**
+ * Property for initializing {@link javax.ejb.embeddable.EJBContainer#PROVIDER}
+ * property
+ * 
+ * @author Levan Tsinadze
+ * @since 0.1.1
+ */
+public class EJBProperties {
 
     public static final String PROVIDER_CLASS = EJBContainerProviderImpl.class
 	    .getName();
 
+    /**
+     * Adds {@link javax.ejb.embeddable.EJBContainer#PROVIDER} property value to
+     * passed {@link Map} of properties
+     * 
+     * @param properties
+     */
     public static void addProvider(Map<?, ?> properties) {
 
 	Map<Object, Object> propertiesMap = ObjectUtils.cast(properties);
 	propertiesMap.put(EJBContainer.PROVIDER, PROVIDER_CLASS);
     }
 
+    /**
+     * Creates {@link Map} of properties and adds
+     * {@link javax.ejb.embeddable.EJBContainer#PROVIDER} property value
+     * 
+     * @return {@link Map} properties with
+     *         {@link javax.ejb.embeddable.EJBContainer#PROVIDER} value
+     */
     public static Map<?, ?> createProperties() {
 
 	Map<?, ?> properties = new HashMap<Object, Object>();
