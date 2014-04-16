9f128294d679812dcbaa4b8e838b965d28211910
levan <levantsinadze@gmail.com>
2014-04-16 19:43:24 +0400

    improved "String ORM" integration utility classes

---
+++ src/main/java/org/lightmare/jpa/hibernate/internal/PersistenceUnitSwapDescriptor.java
@@ -0,0 +1,82 @@
+package org.lightmare.jpa.hibernate.internal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
+import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
+import org.lightmare.utils.ObjectUtils;
+
+public class PersistenceUnitSwapDescriptor extends
+	PersistenceUnitInfoDescriptor implements PersistenceUnitDescriptor {
+
+    private PersistenceUnitTransactionType transactionType;
+
+    private DataSource jtaDataSource;
+
+    private DataSource nonJtaDataSource;
+
+    private List<String> managedClassNames;
+
+    public PersistenceUnitSwapDescriptor(PersistenceUnitInfo persistenceUnitInfo) {
+	super(persistenceUnitInfo);
+	this.nonJtaDataSource = persistenceUnitInfo.getNonJtaDataSource();
+	this.jtaDataSource = persistenceUnitInfo.getJtaDataSource();
+	this.managedClassNames = persistenceUnitInfo.getManagedClassNames();
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+	return transactionType;
+    }
+
+    public void setTransactionType(
+	    PersistenceUnitTransactionType transactionType) {
+	this.transactionType = transactionType;
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+	return jtaDataSource;
+    }
+
+    public void setJtaDataSource(DataSource jtaDataSource) {
+	this.jtaDataSource = jtaDataSource;
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+	return nonJtaDataSource;
+    }
+
+    public void setNonJtaDataSource(Object nonJtaDataSource) {
+
+	if (nonJtaDataSource == null) {
+	    this.nonJtaDataSource = null;
+	} else if (nonJtaDataSource instanceof DataSource) {
+	    this.nonJtaDataSource = ObjectUtils.cast(nonJtaDataSource,
+		    DataSource.class);
+	}
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+	return managedClassNames;
+    }
+
+    public void setManagedClassNames(List<String> managedClassNames) {
+	this.managedClassNames = managedClassNames;
+    }
+
+    public void addClasses(Collection<String> classes) {
+	if (managedClassNames == null) {
+	    managedClassNames = new ArrayList<String>();
+	}
+	managedClassNames.addAll(classes);
+    }
+}
