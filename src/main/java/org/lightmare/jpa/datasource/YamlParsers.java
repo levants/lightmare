f7c9a0b60b0b95983535d9376e36a4d1d5552c2f
levan <levantsinadze@gmail.com>
2014-07-16 16:16:45 +0400

    improved connection pool initialization

--- src/main/java/org/lightmare/jpa/datasource/YamlParsers.java
+++ src/main/java/org/lightmare/jpa/datasource/YamlParsers.java
@@ -4,22 +4,57 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
+import org.lightmare.jpa.datasource.Initializer.ConnectionConfig;
 import org.lightmare.utils.ObjectUtils;
 
 public class YamlParsers {
 
     private static final String DATASOURCES_KEY = "datasources";
 
+    private static final String DATASOURCE_KEY = "datasource";
+
+    private static final Logger LOG = Logger.getLogger(YamlParsers.class);
+
+    private void initDataSource(Map<Object, Object> datasource)
+	    throws IOException {
+
+	Properties propertis = new Properties();
+	Set<Map.Entry<Object, Object>> entrySet = datasource.entrySet();
+	Object key;
+	Object value;
+	for (Map.Entry<Object, Object> entry : entrySet) {
+	    key = entry.getKey();
+	    value = entry.getValue();
+	    if (ConnectionConfig.DRIVER_PROPERTY.name.equals(key)) {
+		value = DriverConfig
+			.getDriverName(ConnectionConfig.DRIVER_PROPERTY.name);
+	    }
+	    propertis.put(key, value);
+	}
+	Initializer.registerDataSource(propertis);
+    }
+
     public void parseYaml(Map<Object, Object> config) throws IOException {
 
 	Object value = config.get(DATASOURCES_KEY);
-	List<Map<Object, Object>> datasources = ObjectUtils.cast(value);
-	Properties propertis;
-	for (Map<Object, Object> datasource : datasources) {
-	    propertis = new Properties();
-	    propertis.putAll(datasource);
-	    Initializer.registerDataSource(propertis);
+	if (ObjectUtils.notNull(value)) {
+	    List<Map<Object, Object>> datasources = ObjectUtils.cast(value);
+	    for (Map<Object, Object> datasource : datasources) {
+		try {
+		    initDataSource(datasource);
+		} catch (IOException ex) {
+		    LOG.error(ex.getMessage(), ex);
+		}
+	    }
+	}
+
+	value = config.get(DATASOURCE_KEY);
+	if (ObjectUtils.notNull(value)) {
+	    Map<Object, Object> datasource = ObjectUtils.cast(value);
+	    initDataSource(datasource);
 	}
     }
 }
