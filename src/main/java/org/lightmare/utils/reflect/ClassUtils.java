004d4dadbabfc0bc65447143cfb9b99c96097105
levan <levantsinadze@gmail.com>
2014-10-23 13:07:26 +0400

    improved utility classes

--- src/main/java/org/lightmare/utils/reflect/ClassUtils.java
+++ src/main/java/org/lightmare/utils/reflect/ClassUtils.java
@@ -49,23 +49,6 @@ import org.lightmare.utils.collections.CollectionUtils;
  */
 public class ClassUtils {
 
-    // default values for primitives
-    private static final byte BYTE_DEF = 0;
-
-    private static final boolean BOOLEAN_DEF = Boolean.FALSE;
-
-    private static final char CHAR_DEF = '\u0000';
-
-    private static final short SHORT_DEF = 0;
-
-    private static final int INT_DEF = 0;
-
-    private static final long LONG_DEF = 0L;
-
-    private static final float FLOAT_DEF = 0F;
-
-    private static final double DOUBLE_DEF = 0D;
-
     // default value for modifier
     private static final int DEFAULT_MODIFIER = 0;
 
@@ -740,34 +723,7 @@ public class ClassUtils {
      * @return {@link Class}<T>
      */
     public static <T> Class<T> getWrapper(Class<?> type) {
-
-	Class<T> wrapper;
-
-	if (type.isPrimitive()) {
-	    if (type.equals(byte.class)) {
-		wrapper = ObjectUtils.cast(Byte.class);
-	    } else if (type.equals(boolean.class)) {
-		wrapper = ObjectUtils.cast(Boolean.class);
-	    } else if (type.equals(char.class)) {
-		wrapper = ObjectUtils.cast(Character.class);
-	    } else if (type.equals(short.class)) {
-		wrapper = ObjectUtils.cast(Short.class);
-	    } else if (type.equals(int.class)) {
-		wrapper = ObjectUtils.cast(Integer.class);
-	    } else if (type.equals(long.class)) {
-		wrapper = ObjectUtils.cast(Long.class);
-	    } else if (type.equals(float.class)) {
-		wrapper = ObjectUtils.cast(Float.class);
-	    } else if (type.equals(double.class)) {
-		wrapper = ObjectUtils.cast(Double.class);
-	    } else {
-		wrapper = ObjectUtils.cast(type);
-	    }
-	} else {
-	    wrapper = ObjectUtils.cast(type);
-	}
-
-	return wrapper;
+	return Primitives.getWrapper(type);
     }
 
     /**
@@ -776,35 +732,7 @@ public class ClassUtils {
      * @param clazz
      * @return Object
      */
-    public static Object getDefault(Class<?> clazz) {
-
-	Object value;
-
-	if (clazz.isPrimitive()) {
-
-	    if (clazz.equals(byte.class)) {
-		value = BYTE_DEF;
-	    } else if (clazz.equals(boolean.class)) {
-		value = BOOLEAN_DEF;
-	    } else if (clazz.equals(char.class)) {
-		value = CHAR_DEF;
-	    } else if (clazz.equals(short.class)) {
-		value = SHORT_DEF;
-	    } else if (clazz.equals(int.class)) {
-		value = INT_DEF;
-	    } else if (clazz.equals(long.class)) {
-		value = LONG_DEF;
-	    } else if (clazz.equals(float.class)) {
-		value = FLOAT_DEF;
-	    } else if (clazz.equals(double.class)) {
-		value = DOUBLE_DEF;
-	    } else {
-		value = null;
-	    }
-	} else {
-	    value = null;
-	}
-
-	return value;
+    public static Object getDefault(Class<?> type) {
+	return Primitives.getDefault(type);
     }
 }
