a7e420728c85f23cd2287cb097078e88444de7aa
levan <levantsinadze@gmail.com>
2016-01-14 00:38:32 +0400

    improving test cases and logging

--- lightmare-ejb/src/test/java/org/lightmare/logger/Configure.java
+++ lightmare-ejb/src/test/java/org/lightmare/logger/Configure.java
@@ -9,20 +9,21 @@ import org.apache.log4j.SimpleLayout;
 public class Configure {
 
     public static void configure() {
-	Logger logger = Logger.getRootLogger();
-	Appender appender = logger.getAppender("CA");
-	logger.removeAppender(appender);
 
-	logger.info("Curr " + System.getProperty("user.dir"));
-	logger.info("This is info should print to console");
-	logger.debug("This is debug should print to console");
+        Logger logger = Logger.getRootLogger();
+        Appender appender = logger.getAppender("CA");
+        logger.removeAppender(appender);
 
-	SimpleLayout layout = new SimpleLayout();
-	ConsoleAppender console = new ConsoleAppender(layout);
+        logger.info("Curr " + System.getProperty("user.dir"));
+        logger.info("This is info should print to console");
+        logger.debug("This is debug should print to console");
 
-	logger.addAppender(console);
-	logger.warn("This is warning should print to file and console");
+        SimpleLayout layout = new SimpleLayout();
+        ConsoleAppender console = new ConsoleAppender(layout);
 
-	logger.setLevel(Level.INFO);
+        logger.addAppender(console);
+        logger.warn("This is warning should print to file and console");
+
+        logger.setLevel(Level.INFO);
     }
 }
